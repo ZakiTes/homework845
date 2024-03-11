22b28ea52e6d905b0139c3ebe11ea6174764473c
ZakiTes <zakites13@outlook.com>
2024-03-11 21:52:42 +0100

    Bucket name

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ const router = new Router();
 
 router.get("/sum/:number1/:number2", async (req, res) => {
   let my_file = await s3.getObject({
-    Bucket: "cyclic-gold-magnificent-springbok-eu-north-1",
+    Bucket: "cyclic-adventurous-hen-hoodie-us-east-1",
     Key: "number.json",
   }).promise()
   const favNumber = JSON.parse(my_file.Body)?.favouriteNumber;
--- save_json.js
+++ save_json.js
@@ -7,7 +7,7 @@ const save = async (favNumber) => {
   console.log("saving");
   await s3.putObject({
     Body: JSON.stringify(favNumber, null, 2),
-    Bucket: "cyclic-gold-magnificent-springbok-eu-north-1",
+    Bucket: "cyclic-adventurous-hen-hoodie-us-east-1",
     Key: "number.json",
   }).promise()
 };
