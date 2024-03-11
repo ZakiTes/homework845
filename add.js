9fff1c72eee25b9496e91ce2cab4156036deefb4
ZakiTes <zakites13@outlook.com>
2024-03-11 20:21:45 +0100

    final

--- add.js
+++ add.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
+
 function add(number1, number2) {
   return number1 + number2;
 }
 
-=======
-function add(number1, number2) {
-  return number1 + number2;
-}
 
->>>>>>> 11d7a2a16e144d768b5b93d9e0d022fd95da86d9
 module.exports = add
